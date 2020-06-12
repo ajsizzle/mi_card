652c190e310dc9632d51b6da2b664313393b13f6
Alex Johnson <ajohnson2389@gmail.com>
2020-06-12 17:35:39 -0400

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                Text(
+                  'Alex Johnson',
+                  style: TextStyle(
+                    fontFamily: 'Satisfy',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.blue.shade400,
+                    fontSize: 16.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.grey.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                        Icons.phone,
+                        color: Colors.blue.shade600,
+                      ),
+                      title: Text(
+                        '+1.321.890.7230',
+                        style: TextStyle(
+                          color: Colors.grey.shade800,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.blue.shade600,
+                    ),
+                    title: Text(
+                      'alex@xanderpages.com',
+                      style: TextStyle(
+                        color: Colors.grey.shade800,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ]
+            ),
+          ),
       ),
-    ),
-  );
+    );
+  }
 }
